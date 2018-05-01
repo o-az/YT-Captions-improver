fa81c3a9233e7709f26215e1d94bc95f78c7df53
binsalamah <binsalamah@users.noreply.github.com>
2018-05-01 11:04:51 -0500

    created .py files (empty for now)
    Signed-off-by: binsalamah <binsalamah@users.noreply.github.com>

---
+++ YTaudio.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',
+        'preferredquality': '192',
+    }],
+    'outtmpl': '/Users/Omar/Desktop/Pythonstuff/Joe.'
+}
+try:
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        # start at <seconds>: end the link with ?t= <second> s (example: ?t=7s)
+        # start at <minutes>: end the link with ?t= <minute> m <second> (example: ?t=1m3s)
+
+        ydl.download(['https://www.youtube.com/watch?v=29oD-AeDwrk'])  # here you can put any link
+except Exception:
+    pass
+# we're not going to worry about other implementations
+# we're only interested in extracting the audio
+
--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# here I should also automate YTaudio.py
+# python3 YTaudio.py &
+# wait
 python3 googleASR.py &
 wait
 python3 punctuation.py
